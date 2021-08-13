fe34689b640f0f2d4803102b2466f025741e5425
root <root@ITUSDURM04664.fitads01.fxdir.net>
2021-08-13 01:09:12 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -x
 
-number_masters=$(docker-compose ps | grep master | grep Up | wc -l)
+number_masters=$(docker-compose ps | grep docker_master | grep Up | wc -l)
 
 hostaddress="$(hostname -i)"
 
 # Clear old rules
 ipvsadm -C
+iptables -t mangle -X
+iptables -t mangle -F
+
 
 
 # Create a Firewall Mark 4505
-#iptables -t mangle -A PREROUTING -d ${hostaddress} -p tcp -m tcp --syn --dport 4505:4506 -j MARK --set-mark 4505
+iptables -t mangle -A PREROUTING -d ${hostaddress} -p tcp -m tcp --syn --dport 4505:4506 -j MARK --set-mark 4505
 
 # Add the Services for both Salt Ports as Firewall Mark
 ipvsadm -A -f 4505 -s rr -p 900
@@ -18,7 +21,7 @@ ipvsadm -A -f 4505 -s rr -p 900
 # Add the Servers to the Service
 for ((i=1;i<=$number_masters;i++)); 
 do 
-   masters[${i}]="$(docker container inspect -f '{{ .NetworkSettings.Networks.saltdocker_salt_net.IPAddress }}' saltdocker_master_${i})"
+   masters[${i}]="$(docker container inspect -f '{{ .NetworkSettings.Networks.saltmaster_docker_salt_net.IPAddress }}' saltmaster_docker_master_${i})"
    ipvsadm -a -f 4505 -r ${masters[${i}]}:0 -m -w 5 # Docker Salt Masters
 done
 
